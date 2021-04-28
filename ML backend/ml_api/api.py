c154b650f822d9cb21ab21afca68d8ba3b909e87
Utkarsh singh <utkarshsingh.1306@gmail.com>
2021-04-28 18:40:48 +0530

    Update api.py

--- ML backend/ml_api/api.py
+++ ML backend/ml_api/api.py
@@ -2,5 +2,5 @@ from firebase import firebase
 
 def firebase_database(report):
 #     print(report)
-    con = firebase.FirebaseApplication('https://hackathon-ff4c0-default-rtdb.firebaseio.com/')        
-    result = con.post('/hackathon-ff4c0-default-rtdb/realdata', report)
+    con = firebase.FirebaseApplication(FIREBASE_URI)        
+    result = con.post(DATABASE_URI, report)
